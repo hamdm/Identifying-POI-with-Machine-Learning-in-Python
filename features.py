992d7b3c06c67d742b38f6e83629117db5d69bf4
Hamd Mazhar <hamdmazharsiddiqui@gmail.com>
2018-02-23 22:32:30 -0600

    Features

---
+++ features.py
@@ -0,0 +1,6 @@
+financialFeatures= ['salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus',
+ 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses',
+ 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 'director_fees']
+
+emailFeatures =  ['to_messages', 'from_poi_to_this_person', 'from_messages',
+                   'from_this_person_to_poi', 'shared_receipt_with_poi']
